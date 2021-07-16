fe3c10d6ec827d02acc7cca74d28ac2005763553
StephanieMFlores21 <floresmariela698@gmail.com>
2021-07-15 22:26:42 -0600

    Formularios MDI
    Creación de menús, formularios, etc.

---
+++ FormulariosMDI/FormMDI.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace FormulariosMDI
+{
+    partial class FormMDI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.productosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaClientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaProductosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaUsuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.usuariosToolStripMenuItem,
+            this.productosToolStripMenuItem,
+            this.clientesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(667, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // usuariosToolStripMenuItem
+            // 
+            this.usuariosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listaUsuariosToolStripMenuItem});
+            this.usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
+            this.usuariosToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.usuariosToolStripMenuItem.Text = "Usuarios";
+            // 
+            // productosToolStripMenuItem
+            // 
+            this.productosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listaProductosToolStripMenuItem});
+            this.productosToolStripMenuItem.Name = "productosToolStripMenuItem";
+            this.productosToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.productosToolStripMenuItem.Text = "Productos";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listaClientesToolStripMenuItem});
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // listaClientesToolStripMenuItem
+            // 
+            this.listaClientesToolStripMenuItem.Name = "listaClientesToolStripMenuItem";
+            this.listaClientesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.listaClientesToolStripMenuItem.Text = "Lista Clientes";
+            this.listaClientesToolStripMenuItem.Click += new System.EventHandler(this.listaClientesToolStripMenuItem_Click_1);
+            // 
+            // listaProductosToolStripMenuItem
+            // 
+            this.listaProductosToolStripMenuItem.Name = "listaProductosToolStripMenuItem";
+            this.listaProductosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.listaProductosToolStripMenuItem.Text = "Lista Productos";
+            this.listaProductosToolStripMenuItem.Click += new System.EventHandler(this.listaProductosToolStripMenuItem_Click_1);
+            // 
+            // listaUsuariosToolStripMenuItem
+            // 
+            this.listaUsuariosToolStripMenuItem.Name = "listaUsuariosToolStripMenuItem";
+            this.listaUsuariosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.listaUsuariosToolStripMenuItem.Text = "Lista Usuarios";
+            this.listaUsuariosToolStripMenuItem.Click += new System.EventHandler(this.listaUsuariosToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // FormMDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 402);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMDI";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMDI_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaUsuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem productosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaProductosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaClientesToolStripMenuItem;
+    }
+}
+
---
+++ FormulariosMDI/FormMDI.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FormulariosMDI
+{
+    public partial class FormMDI : Form
+    {
+        public FormMDI()
+        {
+            InitializeComponent();
+        }
+
+        private void listaUsuariosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //INICIALIZACION DE INSTANCIA
+            UsuariosForm FormularioU = new UsuariosForm();
+
+            //METODO
+            FormularioU.Show();
+        }
+
+        private void FormularioP_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        //DECLARACION
+        private ClientesForm frmClientes;
+
+        private void listaClientesToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (frmClientes == null)
+            {
+                //INSTANCIADO
+                frmClientes = new ClientesForm();
+
+                frmClientes.MdiParent = this;
+
+                //ACTIVACION DEL EVENTO CLOSING (POR MEDIO DE CODIGO)
+                frmClientes.FormClosed += FrmClientes_FormClosed;
+
+                frmClientes.Show();
+            }
+            else
+            {
+                frmClientes.Activate();
+            }
+        }
+
+        //DETECTA QUE SE CERRO EL FORMULARIO Y PERMITE ABRIRLO NUEVAMENTE
+        private void FrmClientes_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            frmClientes = null;
+        }
+
+        //ACTIVACION DE EVENTO CLOSING (POR MEDIO DE EVENTO)
+        private void FormMDI_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("¿Desea cerrar el sistema?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void listaProductosToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            ProductosForm FormularioP = new ProductosForm();
+
+            //FORMULARIO PADRE
+            FormularioP.MdiParent = this;
+
+            FormularioP.Show();
+        }  
+    }
+}
